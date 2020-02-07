be858b164cab5c3e7a20ac46466ea7a8dcb6e38d
黎摄文 <lishewen@gmail.com>
2020-02-07 10:43:37 +0800

    增加DataSet属性

--- Rakor.Blazor.ECharts/Option/EChartsOption.cs
+++ Rakor.Blazor.ECharts/Option/EChartsOption.cs
@@ -39,6 +39,8 @@ namespace Rakor.Blazor.ECharts.Option
         /// </summary>
         public Toolbox Toolbox { set; get; }
 
+        public DataSet DataSet { get; set; }
+
         /// <summary>
         /// 直角坐标系内绘图网格，单个 grid 内最多可以放置上下两个 X 轴，左右两个 Y 轴。可以在网格上绘制折线图，柱状图，散点图（气泡图）。
         /// </summary>
